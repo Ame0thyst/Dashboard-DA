bc7b380c2cdb9583962d6f6f606a43419e987610
Ame0thyst <kokoo15011209@gmail.com>
2024-06-11 02:07:24 +0700

    chore

--- FE/js/index.js
+++ FE/js/index.js
@@ -36,19 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   //lazy loading home sec
-  // scripts.js
-document.addEventListener("DOMContentLoaded", function() {
-  const homeSection = document.querySelector('.home');
 
-  // Membuat elemen gambar baru untuk memuat gambar
-  const img = new Image();
-  img.src = '/assets/NYC-01.webp'; // Ganti dengan URL gambar Anda
-  img.onload = () => {
-      // Setelah gambar dimuat, atur background-image dan tambahkan kelas 'loaded'
-      homeSection.style.backgroundImage = `url('${img.src}')`;
-      homeSection.classList.add('loaded');
-  };
-});
 
   //responsife paragraf home
 
